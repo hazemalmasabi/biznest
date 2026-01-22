54a0a0bdeca2362cee13c728da3e35b238ecfeba
hazemalmasabi <hazem.almasabi@gmail.com>
2026-01-22 19:01:54 +0300

    Add a website for each branch con

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  compiler: {
+    removeConsole: {
+      exclude: [],
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yetajbeaklbahhoeqykw.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
